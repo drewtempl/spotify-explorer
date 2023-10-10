12af7d817fa4b96d8c91db85e66c5dad69adf75f
Drew Templeton <templetonag@appstate.edu>
2023-10-10 03:20:02 -0400

    add cursor pointer

--- client/src/pages/Genre/Genre.tsx
+++ client/src/pages/Genre/Genre.tsx
@@ -3,73 +3,97 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import SongItemList from "../../components/SongItemList";
 
-
-
 export const Genre: React.FC<any> = ({ userData }) => {
-    const [genres, setGenres] = useState<string[]>();
-    const [activeGenres, setActiveGenres] = useState<string[]>([]);
+  const [genres, setGenres] = useState<string[]>();
+  const [activeGenres, setActiveGenres] = useState<string[]>([]);
 
-    const getGenres = () => {
-        axios.get('/api/genres').then(res => {
-            setGenres(res.data.genres);
-        }).catch(error => {
-            console.log(error);
-        })
+  const getGenres = () => {
+    axios
+      .get("/api/genres")
+      .then((res) => {
+        setGenres(res.data.genres);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const createPlaylist = (genres: string[], limit: number): void => {
+    axios
+      .post("/api/playlist/genres", { genres: genres, limit: limit })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleClick = (genre: string) => {
+    if (activeGenres?.includes(genre)) {
+      const arr = activeGenres.filter((el) => el !== genre);
+      setActiveGenres(arr);
+    } else if (activeGenres.length < 5) {
+      setActiveGenres(activeGenres?.concat(genre));
     }
+  };
 
-    const createPlaylist = (genres: string[], limit: number): void => {
-        axios.post('/api/playlist/genres', { genres: genres, limit: limit}).then(res => {
-            console.log(res)
-        }).catch(error => {
-            console.log(error);
-        })
-    }
+  const handleCreate = () => {
+    createPlaylist(activeGenres, 3);
+  };
 
-    const handleClick = (genre: string) => {
-        if (activeGenres?.includes(genre)) {
-            const arr = activeGenres.filter(el => el !== genre);
-            setActiveGenres(arr);
-        } else if (activeGenres.length < 5) {
-            setActiveGenres(activeGenres?.concat(genre));
-        }
-    }
+  useEffect(() => {
+    getGenres();
+  }, []);
 
-    const handleCreate = () => {
-        createPlaylist(activeGenres, 3)
-    }
-
-    useEffect(() => {
-        getGenres();
-    }, [])
-
-    return (
-        <>
-            <SongItemList></SongItemList>       
-
-            <Container
-                sx={{
-                    display: "flex",
-                    "flex-direction": "column",
-                    "align-items": "center",
-                    gap: "15px",
-                    textAlign: "center",
-                }}
-            >
-                <h1>Playlist Creator</h1>
-                <h3>Select up to 5 genres</h3>
-                <Button disabled={!activeGenres.length} variant="contained" color="success" onClick={handleCreate}>Create Playlist!</Button>
-            </Container>
-            <Container>
-                <Grid container spacing={1.5} justifyContent="space-between" >
-                    {genres?.map((genre, index) => {
-                        return (
-                            <Grid item xs={2} sx={{ minWidth: "100px", maxWidth: "130px" }} flexGrow={1}>
-                                <Paper onClick={() => handleClick(genre)} className={activeGenres?.includes(genre) ? 'genre-btn-active' : 'genre-btn'} sx={{ height: "60px", minWidth: "100px" }}><p>{genre}</p></Paper>
-                            </Grid>)
-                    })
-                    }
-                </Grid>
-            </Container>
-        </>
-    )
-}
+  return (
+    <>
+      <Container
+        sx={{
+          display: "flex",
+          "flex-direction": "column",
+          "align-items": "center",
+          textAlign: "center",
+        }}
+      >
+        <h1>Playlist Creator</h1>
+        <h3>Select up to 5 genres</h3>
+        <Button
+          disabled={!activeGenres.length}
+          variant="contained"
+          color="success"
+          onClick={handleCreate}
+          sx={{marginBottom: "30px"}}
+        >
+          Create Playlist
+        </Button>
+      </Container>
+      <Container>
+        <Grid container spacing={1.5} justifyContent="space-between">
+          {genres?.map((genre, index) => {
+            return (
+              <Grid
+                item
+                xs={2}
+                sx={{ minWidth: "100px", maxWidth: "130px" }}
+                flexGrow={1}
+              >
+                <Paper
+                  onClick={() => handleClick(genre)}
+                  className={
+                    activeGenres?.includes(genre)
+                      ? "genre-btn-active"
+                      : "genre-btn"
+                  }
+                  sx={{ height: "60px", minWidth: "100px" }}
+                >
+                  <p>{genre}</p>
+                </Paper>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
+    </>
+  );
+};
