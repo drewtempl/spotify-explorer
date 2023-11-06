22ef3068cc7e69aa0df97745cdf5b5f29864cb27
Drew Templeton <templetonag@appstate.edu>
2023-11-06 18:18:48 -0500

    add prompt form

--- client/src/pages/Prompt/Prompt.tsx
+++ client/src/pages/Prompt/Prompt.tsx
@@ -1,29 +1,101 @@
-import { Box, Button, Container, Grid, Paper, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Paper,
+  SelectChangeEvent,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import CountDropdown from "../../components/CountDropdown";
 
-export const Prompt: React.FC<any> = ({ }) => {
+export const Prompt: React.FC = () => {
+  const [trackCount, setTrackCount] = useState("10");
+  const [prompt, setPrompt] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
 
-    const inputProps = {
-        min: '1',
-    };
+  const countValues = [10, 15, 20, 25, 30];
 
-    return (
-        <Container
-            sx={{
-                display: "flex",
-                "flex-direction": "column",
-                "align-items": "center",
-                textAlign: "center",
-                gap: "30px",
-                pt: '20px'
-            }}
+  const getTracks = () => {
+    axios
+      .get("/api/openai", {
+        params: {
+          prompt: prompt,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleCountChange = (event: SelectChangeEvent) => {
+    setTrackCount(event.target.value);
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    setError(false);
+
+    if (prompt === "") {
+      setError(true);
+    } else {
+      console.log(prompt);
+      getTracks();
+    }
+  };
+
+  const handleChange = (event: any) => {
+    setError(false);
+    setPrompt(event.target.value);
+  };
+
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        "flex-direction": "column",
+        "align-items": "stretch",
+        textAlign: "center",
+        gap: "30px",
+        pt: "20px",
+      }}
+    >
+      <Typography variant="h4">AI playlist generator</Typography>
+      <Typography variant="subtitle1">
+        Generate playlists using a prompt
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit} autoComplete="off">
+        <TextField
+          label="Send a prompt"
+          variant="outlined"
+          fullWidth
+          error={error}
+          onChange={handleChange}
+          sx={{ mb: 3 }}
+        />
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+          gap={3}
         >
-            <Typography variant="h4">AI playlist generator</Typography>
-            <Typography variant="subtitle1">Generate playlists using a prompt</Typography>
-            <TextField label="Send a prompt" variant="outlined" fullWidth />
-            <TextField defaultValue="10" type="number" label="# of songs" variant="outlined" inputProps={inputProps} />
-            <Button variant="contained" color="success">Create playlist</Button>
-        </Container>
-    )
-}
+          <CountDropdown
+            trackCount={trackCount}
+            values={countValues}
+            countHandler={handleCountChange}
+          />
+          <Button variant="contained" type="submit">
+            Generate playlist
+          </Button>
+        </Stack>
+      </Box>
+    </Container>
+  );
+};
